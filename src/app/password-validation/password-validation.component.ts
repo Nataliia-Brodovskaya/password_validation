31b567726cc5765ab313ecff4b4b7f41ccbca2f3
Nataliia <nataliia.brodovskaya@gmail.com>
2023-07-26 10:46:31 +0200

    code refactoring

--- src/app/password-validation/password-validation.component.ts
+++ src/app/password-validation/password-validation.component.ts
@@ -1,23 +1,51 @@
-import { Component, Input, SimpleChange } from '@angular/core';
-
+import { Component, forwardRef, Input, SimpleChange, OnDestroy, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, FormBuilder, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { PasswordValidationService } from './password-validation.service';
 
 @Component({
   selector: 'password-validation',
   templateUrl: './password-validation.component.html',
-  styleUrls: ['./password-validation.component.scss']
+  styleUrls: ['./password-validation.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => PasswordStrongComponent),
+      multi: true
+    }
+  ]
 })
-export class PasswordStrongComponent {
+export class PasswordStrongComponent implements ControlValueAccessor, OnInit, OnDestroy {
   @Input() public passwordToCheck: string;
   complexity0: string;
   complexity1: string;
   complexity2: string;
-
+  form: FormGroup;
   message = '';
 
   private colors = ['lightred', 'yellow', 'green', 'red'];
+  private onChange: (value: any) => void = () => {};
+  private onTouched: () => void = () => {};
+  private subscription: Subscription;
 
-  constructor(private passwordValidationService: PasswordValidationService) { }
+  constructor(
+    private passwordValidationService: PasswordValidationService,
+    private fb: FormBuilder,
+  ) { }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      password: [''],
+    });
+
+    this.subscription = this.form.valueChanges.subscribe(value => {
+      this.onChange(value.password);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
   ngOnChanges(changes: { [propName: string]: SimpleChange }): void {
     const password = changes.passwordToCheck.currentValue;
@@ -26,9 +54,10 @@ export class PasswordStrongComponent {
       const strength = this.passwordValidationService.checkPasswordStrength(password);
       const color = this.getPasswordColor(strength);
       this.setPasswordColor(color.index, color.color);
-
-    this.message = color.index === 4 ? 'Minimum 8 characters' : '';
-    } 
+      this.message = color.index === 4 ? 'Minimum 8 characters' : '';
+    } else {
+      this.message = '';
+    }
   }
 
   getPasswordColor(strength: number) {
@@ -50,9 +79,30 @@ export class PasswordStrongComponent {
     };
   }
 
-  private setPasswordColor(index, color) {
+  private setPasswordColor(index: number, color: string) {
     for (let n = 0; n < index; n++) {
       this['complexity' + n] = color;
     }
   }
+
+  writeValue(value: any): void {
+    this.form.patchValue({ password: value }, { emitEvent: false });
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    if (isDisabled) {
+      this.form.disable();
+    } else {
+      this.form.enable();
+    }
+  }
 }
+
